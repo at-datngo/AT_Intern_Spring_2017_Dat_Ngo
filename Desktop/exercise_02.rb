159e21abc491914d5abf991cde683bf4956a1349
at-datngo <dat.ngo@asiantech.vn>
2017-03-10 17:31:43 +0700

    add ex2

---
+++ Desktop/exercise_02.rb
@@ -0,0 +1,45 @@
+require 'pry'
+require 'active_support/all'
+class Report
+  def initialize(path)
+    @path = path
+  end
+
+  def read_file
+    @report_file = File.new("#{Time.now.strftime("%Y%m%d%H%M%S")}.txt", "w+")
+    f = File.open(@path)
+    f.each do |line|
+      @report_file.write(line + "\n")
+    end
+  end
+
+  def input_report
+    read_file
+    line = 1
+    while true do
+      write_report_to_file(line)
+      line += 1
+      select = gets.chomp
+      case select
+      when 'n'
+        puts "Please check your input file in this path: #{@path}"
+        return
+      else
+      end
+
+    end
+
+  end
+
+  def write_report_to_file(line)
+    title = "Please input your line #{line.ordinalize}: "
+    puts title
+    report_string = gets.chomp
+    @report_file.write(title + "\n")
+    @report_file.write(report_string + "\n")
+    puts "Do you want to input #{(line+1).ordinalize}?"
+  end
+end
+puts "Input your info file path: "
+path = gets.chomp
+Report.new(path).input_report
